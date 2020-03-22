5ef861b70e6bbabd85e51660845e4ed88c883ccf
Will Luft <william.d.luft@gmail.com>
2020-03-22 16:30:51 -0400

    Created card styled component and utils folder

---
+++ src/components/utils/card.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  background: var(--cardBgColor);
+  color: var(--cardColor);
+  border-radius: var(--cardBorderRadius);
+  padding: 10px;
+  box-shadow: var(--elevation-1);
+`
