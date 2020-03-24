ff88ed942942f5ec431396b60619300f1190f69c
Will Luft <william.d.luft@gmail.com>
2020-03-24 17:41:04 -0400

    Created about page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => (
+  <>
+    <Layout>
+      <SEO title="About" />
+      <div>
+        <h1>About</h1>
+      </div>
+    </Layout>
+  </>
+)
+
+export default AboutPage
